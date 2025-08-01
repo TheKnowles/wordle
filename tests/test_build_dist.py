4d31826caf9d0a15cb3b7cbad00b9ff95052a6a7
Knowles Atchison <katchisonjr@gmail.com>
2025-08-01 05:56:55 -0400

    Added test scaffolding.

---
+++ tests/test_build_dist.py
@@ -0,0 +1,9 @@
+import unittest
+from src.build_dist import main
+
+class TestBuildDist(unittest.TestCase):
+  def test_build_dist(self):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
